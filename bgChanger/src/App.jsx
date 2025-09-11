47cc2810430f1541042fadc058c8b47685e1b05b
Manohar Yadav <yadavmanohar78094@gmail.com>
2025-09-11 19:04:43 +0530

    some changes

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -15,9 +15,9 @@ function App() {
     </div>
     
     <div className=" fixed bottom-10 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-full shadow-lg " >
-        <button className=" rounded-lg  mr-3 bg-red-600 p-3" onClick={()=>setColor("red")} >Red </button>
-        <button className=" rounded-lg mr-3 bg-green-600 p-3" onClick={()=>setColor("green")}>Green </button>
-        <button className=" rounded-lg mr-3 bg-blue-600 p-3" onClick={()=>setColor("blue")}>Blue </button>
+        <button className=" rounded-full  mr-3 bg-red-600 p-4" onClick={()=>setColor("red")} >Red   </button>
+        <button className=" rounded-full mr-3 bg-green-600 p-4" onClick={()=>setColor("green")}>Green </button>
+        <button className=" rounded-full mr-3 bg-blue-600 p-4" onClick={()=>setColor("blue")}>Blue </button>
         
     </div>
      
